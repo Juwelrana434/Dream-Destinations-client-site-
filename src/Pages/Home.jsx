678d36037896f1f1df9e994d38f5c6622efe4b31
Juwelrana434 <jewel.rana434@gmail.com>
2024-04-26 22:46:31 +0600

    login section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
-
-
 const Home = () => {
-    return (
-        <div>
-            home
-        </div>
-    );
+  return (
+    <div>
+    
+    </div>
+  );
 };
 
 export default Home;
+{/* <div className="bg-[url('/img/hero-pattern.svg')]">
+  <!-- Your Code -->
+</div> */}
+//style = {{backgroundImage: `url("https://images.unsplash.com/photo-1501183007986-d0d080b147f9?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")`}}
--- src/Pages/Mylist.jsx
+++ src/Pages/Mylist.jsx
@@ -3,7 +3,7 @@
 const Mylist = () => {
     return (
         <div>
-            
+           my list 
         </div>
     );
 };
